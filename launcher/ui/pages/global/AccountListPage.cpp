8239d03e0ed0fea1429e4641764886236062fcbb
Luna <93695520+LunaisLazier@users.noreply.github.com>
2025-02-26 15:19:00 -0600

    Update authlib-injector to point to local doc rather than fjord's

--- launcher/ui/pages/global/AccountListPage.cpp
+++ launcher/ui/pages/global/AccountListPage.cpp
@@ -138,8 +138,8 @@ void AccountListPage::on_actionAddAuthlibInjector_triggered()
     MinecraftAccountPtr account = AuthlibInjectorLoginDialog::newAccount(
         this, tr("Please enter your username (sometimes an email address), password, and the URL of your API server."
                  "<br>"
-                 "See <a href=\"https://github.com/unmojang/FjordLauncher/blob/develop/doc/alternative-auth-servers.md\">this page</a> in "
-                 "the Fjord Launcher repository for a list of common API servers."
+                 "See <a href=\"https://github.com/lunaislazier/ShatteredPrism/blob/develop/doc/alternative-auth-servers.md\">this page</a> in "
+                 "the Shattered Prism repository for a list of common API servers."
                  "<br><br>"
                  "<b>Caution!</b> Your username and password will be sent to the authentication server you specify!"));
 
